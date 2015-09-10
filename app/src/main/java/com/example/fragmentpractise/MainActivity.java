bb29a382ec0bdd83530e0d462a7498aadcacefc6
show111 <1793635590@qq.com>
2015-09-10 18:23:23 +0800

    first

--- app/src/main/java/com/example/fragmentpractise/MainActivity.java
+++ app/src/main/java/com/example/fragmentpractise/MainActivity.java
@@ -1,38 +1,152 @@
 package com.example.fragmentpractise;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements ViewPager.OnPageChangeListener,View.OnTouchListener{
+
+    private ViewPager mViewPager;
+    private View view1,view2,view3;
+    private List<View> list;
+    private LinearLayout pointLayout;
+    private ImageView[] images;
+    private boolean flag;
+    private int count;
+    private int currentItem;
+    private int lastX = 0; // 获得当前的坐标
+    private int currentPoint;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-    }
+        pointLayout = (LinearLayout) findViewById(R.id.llayout);
+        count = pointLayout.getChildCount();
+        for(int i = 0;i < count; i++){
+            images[i] = (ImageView) pointLayout.getChildAt(i);
+            images[i].setEnabled(true);
+            images[i].setTag(i);
+        }
+        currentItem = 0;
+        images[currentItem].setEnabled(false);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        mViewPager = (ViewPager) findViewById(R.id.viewpage);
+        mViewPager.setOnPageChangeListener(this);
+        mViewPager.setOnTouchListener(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        list  = new ArrayList<View>();
+        SharedPreferences prefs = getSharedPreferences("load",MODE_PRIVATE);
+        flag = prefs.getBoolean("loading_flag",false);
+        if(!flag){
+            initPages(inflater);
+        }else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this,MyActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },0);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    private void initPages(LayoutInflater inflater) {
+        view1 = inflater.inflate(R.layout.loading,null);
+        view1.setBackgroundResource(R.mipmap.loding_1);
+        view2 = inflater.inflate(R.layout.loading,null);
+        view2.setBackgroundResource(R.mipmap.loding_2);
+        view3 = inflater.inflate(R.layout.loading,null);
+        view3.setBackgroundResource(R.mipmap.loding_3);
+        list.add(view1);
+        list.add(view2);
+        list.add(view3);
+        mViewPager.setAdapter(pageAdapter);
+    }
+
+    PagerAdapter pageAdapter = new PagerAdapter() {
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return view == o;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(list.get(position));
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(list.get(position));
+            return list.get(position);
+        }
+
+    };
+
+    private void setCurrentPoint(int position) {
+        if(position < 0 || position > count -1 ||currentItem == position){
+            return;
+        }
+
+        images[currentItem].setEnabled(true);
+        images[position].setEnabled(false);
+        currentItem = position;
+    }
+
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {
+
+    }
+
+    @Override
+    public void onPageSelected(int i) {
+        setCurrentPoint(i);
+    }
+
+
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+
+    }
+
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        return false;
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/fragmentpractise/MyActivity.java
@@ -0,0 +1,10 @@
+package com.example.fragmentpractise;
+
+import android.app.Activity;
+
+/**
+ * Created by lhf on 2015/9/10.
+ */
+public class MyActivity extends Activity{
+
+}
